76d37254f77c8c5cb2b14d9fe95c4600b0978f1e
Sushan Shrestha <shresthasushan2004@gmail.com>
2026-06-21 21:45:14 +0545

    final modification

--- airfare.py
+++ airfare.py
@@ -1,38 +1,48 @@
 # Program to calculate airfare:
 
 Fullprice = int(5000)
-FrequentFlyer = True
+
 
 print(f"Flight Ticket Price of single person is:{Fullprice}")
 number = int(input("Enter total numbers of ticket to book:"))
 totalPrice = number*Fullprice
 print(f"Your total is:{totalPrice}")
 
+# frequent flyer club
+FrequentFlyer = input("Are you a frequent flyer?").lower()
+if(FrequentFlyer=="yes"):
+    discount = totalPrice-totalPrice*(20/100)
+    print(f"You've got 20percent off!.Your new total is:{discount}")
+else:
+    print(f"Your total is:{totalPrice}")
+
 # promocode
 PromoCode = input("Enter Promode:")
-if(PromoCode == "first flight"):
-    newPrice =totalPrice - totalPrice*(20/100)
+if(PromoCode == "first flight" and FrequentFlyer=="yes"):
+    newPrice =discount - discount*(20/100)
     print(f"Your total is{newPrice}")
-elif(PromoCode==""):
+elif(PromoCode=="" and FrequentFlyer=="yes"):
+    print(f"Your total is{discount}")
+if(PromoCode == "first flight" and FrequentFlyer=="no"):
+    newPrice = totalPrice - totalPrice*(20/100)
+    print(f"Your total is{newPrice}")
+elif(PromoCode=="" and FrequentFlyer=="no"):
     print(f"Your total is{totalPrice}")
-else:
-    print("Invalid Promocode!!")
+
 # luggage weight
 luggageWeight = int(input("Enter your luggage weight(kg):"))
 if(luggageWeight<=20):
     print("You are good to go!!")
 elif(luggageWeight>20 and PromoCode=="first flight"):
-    print("You have extra weight.Fine amount is Rs.100 per kg")
+    extraWeight = luggageWeight-20
+    print(f"You have {extraWeight}kg more weight.So, fine amount is Rs.100 per kg")
     fine = (luggageWeight-20)*100
     print(f"SO your fine amount is:{fine}")
-    newPrice += fine
-    print(f"Your total is:{newPrice}")
 elif(luggageWeight>20 and PromoCode==""):
-    print("You have extra weight.Fine amount is Rs.100 per kg")
+    extraWeight = luggageWeight-20
+    print(f"You have {extraWeight}kg more weight.So, fine amount is Rs.100 per kg")
     fine = (luggageWeight-20)*100
     print(f"SO your fine amount is:{fine}")
-    totalPrice += fine
-    print(f"Your total is:{totalPrice}")
 else:
     print("Invalid number!!")
 
