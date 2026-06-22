af8608afd61fc65f13b8caa4427bb5979eb24e7d
Sushan Shrestha <shresthasushan2004@gmail.com>
2026-06-22 22:58:56 +0545

    refined

--- vowelconsonent.py
+++ vowelconsonent.py
@@ -1,10 +1,8 @@
-word = input("Enter the word:")
+# program to check if the word is vowel or consonent.
+
+word = input("Enter the word:").lower().strip()
 
 if (word.startswith('a') or word.startswith('e') or word.startswith('i') or word.startswith('o') or word.startswith('u')):
     print(f"{word} is a vowel word")
 else:
-    print(F"{word} is a consonent word")
-
-
-
-    # (word == "a" or word=='e' or word=='i' or word=='o' or word=='u'):
+    print(f"{word} is a consonent word")
